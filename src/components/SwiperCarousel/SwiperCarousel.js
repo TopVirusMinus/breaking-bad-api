5616be62cb408d02771f36dea80f6ac36b44d126
TopVirusMinus <mwmma5000@gmail.com>
2022-08-28 18:44:09 +0200

    First swiper shows 6 random fetched quotes

--- src/components/SwiperCarousel/SwiperCarousel.js
+++ src/components/SwiperCarousel/SwiperCarousel.js
@@ -1,22 +1,55 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
+import React, { useEffect, useState, useRef, useCallback } from "react";
+import { useAsync } from "react-async-hook";
+import axios from "axios";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-
 import SwiperCSS from "./SwiperCarousel.module.css";
-
-// import required modules
 import { Pagination, Autoplay } from "swiper";
 
-function fetchApi(query){
-    
-}
 
-export default function App() {
+export default function SwiperCarousel() {
+  console.log("rerender");
+  
+  const datas = useRef([]);
+  const ref = useRef(false);
+
+  const [calls,  setCalls] = useState(0);
+
+  const fetchData = useCallback(async _ => {
+    datas.current.push(await axios.get("https://www.breakingbadapi.com/api/quote/random"))
+    setCalls(prevCalls => prevCalls + 1);
+    return datas;
+  }, []); 
+  
+  let quotes = <SwiperSlide className={SwiperCSS.swiperSlide}>Loading data...</SwiperSlide>;
+
+  useEffect(()=>{
+    if(calls < 6){
+      if(ref.current){
+        fetchData();
+      }
+    
+      return ()=>{
+        ref.current = true;
+      }
+    }
+  })
+
+  //console.log(datas.current);
+  console.log(calls); 
+ const fillCarousel = useCallback(()=>{
+    console.log("mapping");
+    quotes = datas.current.map((res)=>{
+      return <SwiperSlide key={res.data[0].quote_id} className={SwiperCSS.swiperSlide}> {res.data[0].quote} </SwiperSlide>
+    })
+  });
+
+  if(calls >= 6){
+    fillCarousel();
+  }
+
   return (
     <>
       <Swiper
@@ -31,15 +64,7 @@ export default function App() {
         modules={[Pagination, Autoplay]}
         className={SwiperCSS.swiper}
       >
-        <SwiperSlide className={SwiperCSS.swiperSlide}>Slide 1</SwiperSlide>
-        <SwiperSlide className={SwiperCSS.swiperSlide}>Slide 2</SwiperSlide>
-        <SwiperSlide className={SwiperCSS.swiperSlide}>Slide 3</SwiperSlide>
-        <SwiperSlide className={SwiperCSS.swiperSlide}>Slide 4</SwiperSlide>
-        <SwiperSlide className={SwiperCSS.swiperSlide}>Slide 5</SwiperSlide>
-        <SwiperSlide className={SwiperCSS.swiperSlide}>Slide 6</SwiperSlide>
-        <SwiperSlide className={SwiperCSS.swiperSlide}>Slide 7</SwiperSlide>
-        <SwiperSlide className={SwiperCSS.swiperSlide}>Slide 8</SwiperSlide>
-        <SwiperSlide className={SwiperCSS.swiperSlide}>Slide 9</SwiperSlide>
+       {quotes}
       </Swiper>
     </>
   );
